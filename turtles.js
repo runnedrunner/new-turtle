c6e1cd85b33e273b661856aacd34d7f7a151522a
runnedrunner <auszhou@nuevaschool.org>
2016-12-07 09:23:21 -0800

    Classadoo autocommit

--- turtles.js
+++ turtles.js
@@ -9,6 +9,9 @@ $( "#position" ).click(function() {
     left: position.x, 
     bottom: position.y,
   });
+  complete : function() {
+    insertLine(origPosition, origHeading, distance)
+  }
   console.log(expos + eypos);
 });
 var turtle = $("#turtle");
