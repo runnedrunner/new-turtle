89694be3ab97fe930f651ea57c58781f87afa9c2
runnedrunner <auszhou@nuevaschool.org>
2016-12-07 08:54:19 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function polygonplz (sides) {
   }
 }
 $( "#buttonthing" ).click(function() {
-  var sides = $("#numberz").value;  
+  var sides = $("#numberz").val();  
   console.log(sides);
   for (var i = 0; i < sides; i++) {
       goForward(400/sides);
