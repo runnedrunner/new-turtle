2c772fdf074088364deda540ca8399ab3c013eb9
runnedrunner <auszhou@nuevaschool.org>
2016-12-14 09:14:49 -0800

    Classadoo autocommit

--- turtles.js
+++ turtles.js
@@ -73,6 +73,7 @@ document.onkeydown = checkKey;
 function checkKey(e) {
 
   e = e || window.event;
+  e.preventDefault();
   if (e.keyCode == '32') {
     console.log("space!!");
     if (pendown) {
